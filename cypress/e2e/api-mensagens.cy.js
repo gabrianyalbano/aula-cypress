45d496921ca2a7800aa6b21b12c80d8d85d03913
Gabriany Albano <gabriany@Outlook.com.br>
2025-04-21 14:10:36 -0300

    protegendo o token

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,10 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc0NTAyNTEyMiwiZXhwIjoxNzQ1Mjg0MzIyfQ.bn2map9OIEKZaICYAqzv9tngAIxFea86N1R34wBeG_8`
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: {authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
